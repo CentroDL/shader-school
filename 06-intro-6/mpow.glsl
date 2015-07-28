ac8907f000b5099d9918009c828aa2593ba256c6
Dennis Liaw <dennis.liaw@gmail.com>
2015-07-28 14:27:37 -0400

    adds solution to matrix exercise

--- 06-intro-6/mpow.glsl
+++ 06-intro-6/mpow.glsl
@@ -1,13 +1,20 @@
 mat2 matrixPower(highp mat2 m, int n) {
-  
-  //Raise the matrix m to nth power
 
-  // For example:
-  //
-  //  matrixPower(m, 2) = m * m
-  //
+  // highp mat2 result = mat2(1.0) * m;
 
-  return mat2(1.0);  
+  // for(int i=0; i < n;i++){
+  //    result = matrixCompMult(result,m);
+  // }
+
+  // return result;
+    highp mat2 p = mat2(1.0) * m;
+  for(int i = 1; i < 16; i++) {
+    if (i == n) return p;
+    p *= m;
+  }
+
+  return p;
+
 }
 
 //Do not change this line or the name of the above function
